8c8791120d0196b3619a38a640742168dae4792a
Stephan Hesse <tchakabam@gmail.com>
2018-03-03 16:31:20 +0100

    enable source-maps for all builds

--- webpack.config.js
+++ webpack.config.js
@@ -166,7 +166,8 @@ const multiConfig = [
       libraryTarget: 'umd',
       libraryExport: 'default'
     },
-    plugins: getPluginsForConfig('main', true)
+    plugins: getPluginsForConfig('main', true),
+    devtool: 'source-map'
   },
   {
     name: 'light',
@@ -200,7 +201,8 @@ const multiConfig = [
     resolve: {
       alias: getAliasesForLightDist()
     },
-    plugins: getPluginsForConfig('light', true)
+    plugins: getPluginsForConfig('light', true),
+    devtool: 'source-map'
   }
 ].map(config => clone(baseConfig, config));
 
